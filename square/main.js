363f4d5a959b2642487f044b01cf87f3d76e431e
nil3190 <nilluu643d@gmail.com>
2019-03-29 19:50:20 +0530

    homework:square and square root

---
+++ square/main.js
@@ -0,0 +1,13 @@
+function square(){
+    var no;
+    no=Number(document.getElementById("nsquare").value);
+
+    alert(no*no);
+    }
+    function squareroot(){
+        var no1;
+        no1=Number(document.getElementById("nsquareroot").value);
+        var value =Math.sqrt(no1);
+        alert(value);
+        }
+        
